7b9521a81a4b5fc5d6763dab9c66a7e9bf8d27df
Enimofe Odujirin <daemonargo@protonmail.com>
2022-11-12 19:18:50 -0500

    new day

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/thebaggieboy/Documents/Snippets/django_archives/django-ecommerce/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
