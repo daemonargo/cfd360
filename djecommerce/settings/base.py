dd5ebb05a17522c94e9ea77c116c20246d0f9f9c
Enimofe Odujirin <daemonargo@protonmail.com>
2022-11-18 12:10:46 -0500

    Added heroku config files ish

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'django.middleware.common.CommonMiddleware',   
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
