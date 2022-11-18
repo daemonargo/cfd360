59e3bfffbd067a345eeee8eccdc7bb6d623651c5
Enimofe Odujirin <daemonargo@protonmail.com>
2022-11-18 07:54:16 -0500

    dhd

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -20,3 +20,24 @@ DATABASES = {
     }
 }
 """
+
+if DEBUG == True:
+    """DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}"""
+else:
+    """DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cfd360_user',
+        'USER': 'cfd360',
+        'PASSWORD': 'RKzduwRNAORhspPck6jSFOIO8ciAafkg',
+        'HOST': 'dpg-cdo4u19a6gdooi7q29v0-a',
+        'PORT': '5432'
+    }
+}
+"""
+    print('Debug is false')
