05355bdaec91ee15abab3c71f4f1267fb209a69b
Enimofe Odujirin <daemonargo@protonmail.com>
2022-11-18 11:48:16 -0500

    Added heroku config files ish

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -1,43 +1,23 @@
 from .base import *
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cfd360.net', 'www.cfd360.net', 'cfd360.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cfd360.net', 'www.cfd360.net', 'cfd360.onrender.com', 'cfd360.herokuapp.com', 'www.cfd360.herokuapp.com']
 
 DEBUG = True
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cfd360_user',
-        'USER': 'cfd360',
-        'PASSWORD': 'RKzduwRNAORhspPck6jSFOIO8ciAafkg',
-        'HOST': 'dpg-cdo4u19a6gdooi7q29v0-a',
-        'PORT': '5432'
-    }
-}
-"""
 
 if DEBUG == True:
-    """DATABASES = {
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
+}
 else:
-    """DATABASES = {
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cfd360_user',
-        'USER': 'cfd360',
-        'PASSWORD': 'RKzduwRNAORhspPck6jSFOIO8ciAafkg',
-        'HOST': 'dpg-cdo4u19a6gdooi7q29v0-a',
+        'NAME': 'd2u7n0iq6epklf',
+        'USER': 'vnidaawijldlcr',
+        'PASSWORD': '2c765c6776fd6d783696d8e2f982dd67a6ca22205086d78b7d45b42644c42955',
+        'HOST': 'ec2-35-168-194-15.compute-1.amazonaws.com',
         'PORT': '5432'
     }
 }
-"""
-    print('Debug is false')
--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -1,25 +1,7 @@
 from .base import *
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cfd360.net', 'www.cfd360.net', 'cfd360.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cfd360.net', 'www.cfd360.net', 'cfd360.onrender.com', 'cfd360.herokuapp.com', 'www.cfd360.herokuapp.com']
 
 DEBUG = False
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cfd360_user',
-        'USER': 'cfd360',
-        'PASSWORD': 'RKzduwRNAORhspPck6jSFOIO8ciAafkg',
-        'HOST': 'dpg-cdo4u19a6gdooi7q29v0-a',
-        'PORT': '5432'
-    }
-}
-"""
 
 if DEBUG == True:
     DATABASES = {
@@ -32,10 +14,10 @@ else:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cfd360_user',
-        'USER': 'cfd360',
-        'PASSWORD': 'RKzduwRNAORhspPck6jSFOIO8ciAafkg',
-        'HOST': 'dpg-cdo4u19a6gdooi7q29v0-a',
+        'NAME': 'd2u7n0iq6epklf',
+        'USER': 'vnidaawijldlcr',
+        'PASSWORD': '2c765c6776fd6d783696d8e2f982dd67a6ca22205086d78b7d45b42644c42955',
+        'HOST': 'ec2-35-168-194-15.compute-1.amazonaws.com',
         'PORT': '5432'
     }
 }
