61e5f8c6de6cd1adb1a138fe6722f6a6dcc32ec0
Enimofe Odujirin <daemonargo@protonmail.com>
2022-11-19 18:57:21 -0500

    Updated some shii

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -2,18 +2,18 @@ from .base import *
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cfd360.net', 'www.cfd360.net', 'cfd360.onrender.com', 'cfd360.herokuapp.com', 'www.cfd360.herokuapp.com']
 
 DEBUG = True
-'''if DEBUG == True:
+if DEBUG == True:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
-DATABASES = {
+
+'''DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd2u7n0iq6epklf',
         'USER': 'vnidaawijldlcr',
         'PASSWORD': '2c765c6776fd6d783696d8e2f982dd67a6ca22205086d78b7d45b42644c42955',
@@ -21,3 +21,4 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+'''
