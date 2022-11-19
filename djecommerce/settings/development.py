05eb648a7c971096ae22fe2e1235dcc04b9bcf00
Enimofe Odujirin <daemonargo@protonmail.com>
2022-11-19 10:32:20 -0500

    latedt

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -2,7 +2,6 @@ from .base import *
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cfd360.net', 'www.cfd360.net', 'cfd360.onrender.com', 'cfd360.herokuapp.com', 'www.cfd360.herokuapp.com']
 
 DEBUG = True
-
 """if DEBUG == True:
     DATABASES = {
     'default': {
