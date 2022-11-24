b99ff4d7c18439ea8102c17d37baf8a4d859007a
Enimofe Odujirin <daemonargo@protonmail.com>
2022-11-24 09:43:30 -0500

    removed heroku staticfiles

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -3,15 +3,14 @@ from .base import *
 
 DEBUG = False
 
-"""if DEBUG == True:
+if DEBUG == False:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-else:
-"""
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -22,7 +21,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+'''
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
